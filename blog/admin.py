cbfdee381f3bc934c3814cc6c3b5436ba54873bd
Bekzod <bekzod@gamil.com>
2023-09-28 17:08:37 +0500

    forms.py and views.py and urls.py and urls.py

--- blog/admin.py
+++ blog/admin.py
@@ -15,24 +15,59 @@ class CourseAdmin(admin.ModelAdmin):
     )
 
     list_display = ['title','about','price','duration','image','phone']
-    
 
 
-admin.site.register(Contact)
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    fields = (
+        'name',
+        'image',
+        'level',
+        'about',
+        'phone',
+    )
 
+    list_display = ['name', 'image', 'level', 'about', 'phone']
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fields = (
+        'picture',
+        'title',
+        'about',
+        'vidio',
+    )  
+
+    list_display = ['picture', 'title', 'about', 'vidio']
 
 @admin.register(Level)
-class LevelAdmin(admin.ModelAdmin):
+class LevelAdmin(admin.Model.Admin):
     fields = (
-        'image',
-        'title',
-        'reading',
-        'listening',
-        'writing',
-        'speaking',
-    )     
+        'lider',
+    )    
 
-    list_display = ['image', 'title', 'reading', 'listening', 'writing', 'speaking']
+    list_display = ['lider']
 
+@admin.register(Students)
+class StudentsAdmin(admin.Model.Admin):
+    fields = (
+        'fullname',
+        'email',
+        'number',
+        'extnumber',
+        'day',
+        'stu_day',
+    )    
 
- 
+    list_display = ['fullname', 'email', 'number', 'extnmber', 'day', 'stu_day']
+
+@admin.register(Contact)
+class ContactAdmin(admin.Model.Admin):
+    fields = (
+        'tel_nomer',
+        'email',
+        'level',
+        'message',
+    )    
+
+    list_display = ['tel_nomer', 'email', 'level', 'message']
